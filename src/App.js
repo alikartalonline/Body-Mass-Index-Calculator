4774ba9935b050539719d7f53d466310c3dda1d3
Ali Kartal <ali_kartal91@hotmail.com>
2022-11-08 16:58:41 +0300

    update

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+// COMPONENTS
+import Homepage from './components/Homepage';
+
+function App() {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+};
+
+export default App;
