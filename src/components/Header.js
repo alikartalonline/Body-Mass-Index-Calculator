ff37c150ecf580295b6f355faf8090fad975b413
Ali Kartal <ali_kartal91@hotmail.com>
2022-11-19 16:05:17 +0300

    "update - final with at least 3 problems."

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,16 +7,16 @@ function Header({ page }) {
 
             <h1>Vücut Kitle Endeksi Hesaplama</h1>
             
-            <p className='mt-3 col-8'>Aşağıdaki hesaplama aracında beden kitle endeksini öğrenmek istediğiniz kişinin <span>"cinsiyetini"</span> seçtikten sonra <span className=''>"cm"</span> boyu ile <span>"kg"</span> cinsinden ağırlığını giriniz ve "Calculate" butonuna basınız. </p>
+            <p className='mt-3 col-8'>Aşağıdaki hesaplama aracında beden kitle endeksini öğrenmek istediğiniz kişinin <b className='text-danger'>"Cinsiyetini"</b> <em className='h6'>("Gender")</em> seçtikten sonra <kbd>"cm"</kbd> boyu ile  <kbd>"kg"</kbd> cinsinden ağırlığını giriniz ve <b className='text-primary'>"Calculate"</b> butonuna basınız. </p>
 
             {
                 page === false ?
-                    <div class="alert alert-primary d-flex align-items-center" role="alert">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" height={30} aria-label="Warning:">
+                    <div className="alert alert-primary d-flex align-items-center mt-5" role="alert">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" height={30} aria-label="Warning:">
                             <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
                         </svg>
                         <div>
-                            Hesaplama ekranına geri dönmek isterseniz <span>"Reset"</span> butonuna basınız ve tekrardan aynı işlemleri tekrarlayabilirsiniz.
+                            Hesaplama ekranına geri dönmek isterseniz <button type="button" className='btn btn-danger btn-sm' disabled>Reset</button> butonuna basınız ve tekrardan aynı işlemleri tekrarlayabilirsiniz.
                         </div>
                     </div> : null
             }
